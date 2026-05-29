da08802fef73797a36743da345236205d1358f9b
arseny <arsenyosemin@gmail.com>
2026-05-28 21:55:45 -0500

    heroku config

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 const buildPath = path.join(__dirname, 'dist'); // Use 'web-build' if using an older Expo SDK
 app.use(express.static(buildPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(buildPath, 'index.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
